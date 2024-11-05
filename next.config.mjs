a954f41d6a93398ddf6fb45c64f634ff04d4aa21
Anil Shrestha <61676145+anilshrestha2400@users.noreply.github.com>
2024-11-05 17:48:14 +0545

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: "build",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
+  // distDir: "build",
+  // generateBuildId: async () => {
+  //   if (process.env.BUILD_ID) {
+  //     return process.env.BUILD_ID;
+  //   } else {
+  //     return `${new Date().getTime()}`;
+  //   }
+  // },
   images: {
     remotePatterns: [
       {
