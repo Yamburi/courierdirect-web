acecc61d5e7d9a706e0da9f35bce9ede56350e60
Anil Shrestha <61676145+anilshrestha2400@users.noreply.github.com>
2024-11-06 21:12:35 +0545

    changed version to 13.5.4

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  // distDir: "build",
-  // generateBuildId: async () => {
-  //   if (process.env.BUILD_ID) {
-  //     return process.env.BUILD_ID;
-  //   } else {
-  //     return `${new Date().getTime()}`;
-  //   }
-  // },
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -29,4 +21,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
